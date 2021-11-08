fe2bdaa033e5a05b7b58b0e76d28077efdaefe01
Anderson Silva <andssilva.23@gmail.com>
2021-11-08 18:08:56 -0400

    feat: :rocket: cria a função updateTask no tasksController

--- server/src/controllers/tasksController.ts
+++ server/src/controllers/tasksController.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Response, Request } from 'express';
-import { create, getAll } from '../services/tasksService';
+import { create, getAll, update } from '../services/tasksService';
 
 export const createTask = async (request: Request, response: Response, next: NextFunction) => {
   const { user } = request;
@@ -20,6 +20,18 @@ export const getAllTasks = async (request: Request, response: Response, next: Ne
     const getTasks = await getAll(_id);
 
     return response.status(getTasks.status).json(getTasks.message);
+  } catch (err) {
+    return next(err);
+  }
+};
+
+export const updateTask = async (request: Request, response: Response, next: NextFunction) => {
+  const { id } = request.params;
+
+  try {
+    const setTask = await update(id, request.body);
+
+    return response.status(setTask.status).json(setTask.message);
   } catch (err) {
     return next(err);
   }
