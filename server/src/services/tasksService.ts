1c2b618c1cb568ab479f74b6fa8dfccda9e7eef4
Anderson Silva <andssilva.23@gmail.com>
2021-11-08 09:57:56 -0400

    feat: :rocket: cria a função getAll no tasksService

--- server/src/services/tasksService.ts
+++ server/src/services/tasksService.ts
@@ -1,12 +1,15 @@
 import verifyTasksField from '../auth/verifyTasksField';
-import { createTask } from '../models/tasksModel';
+import { createTask, getAllTasks } from '../models/tasksModel';
 import { Task, User } from '../utils/interfaces';
-import { CREATED } from '../utils/successStatus';
+import { CREATED, STATUS_OK } from '../utils/successStatus';
 
-const create = async (tasksObj: Task, user: User) => {
+export const create = async (tasksObj: Task, user: User) => {
   await verifyTasksField(tasksObj);
   const created = await createTask(tasksObj, user);
   return { status: CREATED, message: created };
 };
 
-export default create;
+export const getAll = async (userId: string) => {
+  const arrayTasks = await getAllTasks(userId);
+  return { status: STATUS_OK, message: arrayTasks };
+};
