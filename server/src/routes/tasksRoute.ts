a8eb53089ff147920d2b0f94ec50b8536dcddf7e
Anderson Silva <andssilva.23@gmail.com>
2021-11-09 01:51:21 -0400

    feat: :rocket: cria a rota que deleta uma task

--- server/src/routes/tasksRoute.ts
+++ server/src/routes/tasksRoute.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import verifyToken from '../middlewares/verifyToken';
-import { createTask, getAllTasks, updateTask } from '../controllers/tasksController';
+import {
+  createTask, deleteTask, getAllTasks, updateTask,
+} from '../controllers/tasksController';
 
 const router = express.Router();
 
 router.post('/', verifyToken, createTask);
 router.get('/', verifyToken, getAllTasks);
 router.put('/:id', verifyToken, updateTask);
+router.delete('/:id', verifyToken, deleteTask);
 
 export default router;
